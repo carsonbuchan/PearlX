93e31b8887a10217fc6fc2f7abb6b24d29587a47
carsonbuchan <carsonbuchan@gmail.com>
2021-12-10 15:53:23 -0500

    Tweaked and added more to the site

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,9 +4,24 @@ var revealElements = document.getElementsByClassName("cardPic");
 		for (var i=0; i<revealElements.length; i++) { // create a scene for each element
 			new ScrollMagic.Scene({
 								triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-								offset: 100,												 // start a little later
-								triggerHook: 0.9,
+								offset: 150, // start a little later
+								triggerHook: 0.5,
 							})
 							.setClassToggle(revealElements[i], "visible") // add class toggle
 							.addTo(controller);
 		}
+		var revealElements = document.getElementsByClassName("cardPicLeft");
+		for (var i=0; i<revealElements.length; i++) { // create a scene for each element
+			new ScrollMagic.Scene({
+								triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+								offset: 150, // start a little later
+								triggerHook: 0.5,
+							})
+							.setClassToggle(revealElements[i], "visible") // add class toggle
+							.addTo(controller);
+		}
+
+			// var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 300})
+			// 				.setPin("#pin1")
+			// 				.addIndicators({name: "1 (duration: 300)"}) // add indicators (requires plugin)
+			// 				.addTo(controller);
