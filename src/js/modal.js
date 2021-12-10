e140609d5b6c38f9d6971f418bc8ed9d22f4605f
carsonbuchan <carsonbuchan@gmail.com>
2021-12-09 19:28:11 -0500

    card parallaxing done

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,12 @@
-<script type="text/javascript" src="js/jquery.scrollmagic.min.js"></script>
+var controller = new ScrollMagic.Controller();
+
+var revealElements = document.getElementsByClassName("cardPic");
+		for (var i=0; i<revealElements.length; i++) { // create a scene for each element
+			new ScrollMagic.Scene({
+								triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+								offset: 100,												 // start a little later
+								triggerHook: 0.9,
+							})
+							.setClassToggle(revealElements[i], "visible") // add class toggle
+							.addTo(controller);
+		}
