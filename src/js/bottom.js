edeefbb4a1420e77b5fcf78cc811f58398a4cb51
carsonbuchan <carsonbuchan@gmail.com>
2021-12-12 18:08:57 -0500

    removed bottom portion, need to swap portraits

--- src/js/bottom.js
+++ src/js/bottom.js
@@ -8,4 +8,13 @@
                 } 
             });
         });
-      
+        $(function() {
+            var portrait = $(".portraitX");
+            $(window).scroll(function() {    
+                var scroll = $(window).scrollTop();
+            
+                if (scroll >= 8000) {
+                    portrait.removeClass('portraitX').addClass("portraitChildX");
+                } 
+            });
+        });
