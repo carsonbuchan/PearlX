0fa3cee4a906c06ffacb216342e4f5c2fa4e00fa
carsonbuchan <carsonbuchan@gmail.com>
2021-12-12 13:57:23 -0500

    trying out the bottom, need to change pictures with videos

--- src/js/modal.js
+++ src/js/modal.js
@@ -21,7 +21,21 @@ var revealElements = document.getElementsByClassName("cardPic");
 							.addTo(controller);
 		}
 
-			// var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 300})
-			// 				.setPin("#pin1")
-			// 				.addIndicators({name: "1 (duration: 300)"}) // add indicators (requires plugin)
-			// 				.addTo(controller);
+
+		// $(function () { // wait for document ready
+		// 	// build scene
+		// 	var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 300})
+		// 					.setPin("#pin1")
+		// 					.addIndicators({name: "1 (duration: 300)"}) // add indicators (requires plugin)
+		// 					.addTo(controller);
+		// });
+
+
+		// $(document).scroll(function(){
+		// 	var scrollTop = $(".bottomHero").scrollTop();
+		// 	if(scrollTop > 150){
+		// 		$(".bottomHeroTrans").fadeIn();
+		// 	}
+		// 	else
+		// 		$(".bottomHeroTrans").fadeOut();
+		// })
